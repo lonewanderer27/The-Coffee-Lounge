7ad74386298f4a66724f480936cd108f89fc0860
lonewanderer27 <adrianejames27@gmail.com>
2023-08-24 01:28:27 +0800

    app:feat: improve onboarding

--- apps/app/src/pages/Onboarding.tsx
+++ apps/app/src/pages/Onboarding.tsx
@@ -1,6 +1,8 @@
+import { ComboBox, Item } from "../components/ComboBox";
 import {
   FieldValue,
   doc,
+  getDoc,
   getFirestore,
   serverTimestamp,
   setDoc,
@@ -9,20 +11,29 @@ import {
   IonButton,
   IonCol,
   IonContent,
+  IonHeader,
   IonImg,
   IonInput,
   IonPage,
   IonRow,
   IonSpinner,
-  IonText,
+  IonTitle,
+  IonToolbar,
   useIonAlert,
   useIonRouter,
 } from "@ionic/react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { getAuth, updateProfile } from "firebase/auth";
 import { memo, useState } from "react";
+import {
+  useDocumentData,
+  useDocumentDataOnce,
+} from "react-firebase-hooks/firestore";
 
 import { FirebaseError } from "firebase/app";
+import { UserConvert } from "../converters/user";
+import { UserType } from "../types";
+import { isPlatform } from "@ionic/react";
 
 interface IFormInput {
   nickname: string;
@@ -36,11 +47,31 @@ function Onboarding() {
   const auth = getAuth();
   const router = useIonRouter();
 
+  const userRef = doc(
+    db,
+    "users",
+    auth.currentUser?.uid ?? "Loading"
+  ).withConverter(UserConvert);
+  const [values] = useDocumentDataOnce(userRef);
+
+  console.info(values);
+
   const {
     register,
     handleSubmit,
-    formState: { errors, isValid, isValidating },
-  } = useForm<IFormInput>();
+    setValue,
+    watch,
+    formState: { isValid, isValidating },
+  } = useForm<IFormInput>({
+    defaultValues: async () => {
+      const res = await getDoc(userRef);
+      return (
+        res.data() ?? {
+          nickname: "",
+        }
+      );
+    },
+  });
   const [loading, setLoading] = useState(false);
   const [presentAlert] = useIonAlert();
 
@@ -80,22 +111,22 @@ function Onboarding() {
 
   return (
     <IonPage>
-      <IonContent fullscreen>
-        <div className="h-full flex">
+      <IonContent className="ion-padding">
+        <div className="p-4">
+          <IonImg
+            src={`/people/${
+              watch("gender") == "Female" ? "woman" : "man"
+            }_alt.png`}
+            className="my-5 w-2/4 ml-[-20px]"
+          />
+          <h6 className="font-bold">Before you start</h6>
+          <p>
+            To personalize your orders, we would like to ask you a few things.
+          </p>
           <form
-            className="ion-padding flex flex-col justify-center items-center"
+            className="flex flex-col justify-center items-center mt-8"
             onSubmit={handleSubmit(onSubmit)}
           >
-            <IonImg
-              src="/assets/Success.png"
-              className="w-2/3 ion-margin-bottom"
-            />
-            <IonText className="ion-margin-vertical text-center">
-              <span className="font-bold">
-                To personalize your orders, we'd just like to ask you two
-                things. Rest assured this information is kept private.
-              </span>
-            </IonText>
             <IonInput
               label="Nickname"
               labelPlacement="floating"
@@ -104,31 +135,61 @@ function Onboarding() {
               {...register("nickname", { required: true })}
             />
             <IonRow>
-              <IonCol className="pl-0 pt-0">
-                <IonInput
-                  label="Gender (Optional)"
-                  labelPlacement="floating"
-                  fill="outline"
-                  className="mt-4"
-                  {...register("gender", { required: false })}
-                />
-              </IonCol>
-              <IonCol className="pr-0 pt-0">
-                <IonInput
-                  label="Pronouns (Optional)"
-                  labelPlacement="floating"
-                  fill="outline"
-                  className="mt-4"
-                  {...register("pronouns", { required: false })}
-                />
-              </IonCol>
+              <div>
+                <div className="mt-2">
+                  <ComboBox
+                    inputValue={watch("gender")}
+                    label="Gender (Optional)"
+                    {...register("gender", { required: false })}
+                    onInputChange={(v) => {
+                      if (v.includes("not say")) {
+                        setValue("gender", "")
+                      } else {
+                        setValue("gender", v);
+                      }
+                    }}
+                  >
+                    <Item key="Male">Male</Item>
+                    <Item key="Female">Female</Item>
+                    <Item key="Non Binary">Non-Binary</Item>
+                    <Item key="I'd rather not say">I'd rather not say</Item>
+                  </ComboBox>
+                </div>
+              </div>
+              <div>
+                <div className="mt-2">
+                  <ComboBox
+                    inputValue={watch("pronouns")}
+                    label="Pronouns (Optional)"
+                    {...register("pronouns", { required: false })}
+                    onInputChange={(v) => {
+                      if (v.includes("not say")) {
+                        setValue("pronouns", "")
+                      } else {
+                        setValue("pronouns", v);
+                      }
+                    }}
+                  >
+                    <Item key="He/Him">He/Him</Item>
+                    <Item key="She/Her">She/Her</Item>
+                    <Item key="They/Them">They/Them</Item>
+                    <Item key="I'd rather not say">I'd rather not say</Item>
+                  </ComboBox>
+                </div>
+              </div>
             </IonRow>
             <IonButton
-              className="ion-margin-top w-full"
+              className="ion-margin-top w-full mt-8"
               type="submit"
               disabled={!isValid || isValidating}
             >
-              {loading ? <IonSpinner /> : "Continue"}
+              {loading ? (
+                <IonSpinner
+                  className={`${!isPlatform("ios") && "animate-spin"}`}
+                />
+              ) : (
+                "Continue"
+              )}
             </IonButton>
           </form>
         </div>
